03f76500a23e79c387d15d644dee756f264a8f40
Charalambos <charith12.CT@gmail.com>
2024-11-22 17:37:10 +0200

    comment put

--- epl343project/epl343project/asgi.py
+++ epl343project/epl343project/asgi.py
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'epl343project.settings')
 
 application = get_asgi_application()
+
+#This is my comment
