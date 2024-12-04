b2ebe7939681639cbc8cd2a76ba0b4ea6ef71e34
NestorasGreg <ngrego03@ucy.ac.cy>
2024-12-05 00:17:45 +0200

    sidebar things again

--- user_page.php
+++ user_page.php
@@ -79,12 +79,7 @@ try {
                             class="<?= basename($_SERVER['PHP_SELF']) == 'create_tasks_user.php' ? 'active' : ''; ?>">Create Task</a>
                     </li>
                 <?php endif; ?>
-                <li>
-                    <a href="#settings"
-                        class="<?= basename($_SERVER['PHP_SELF']) === '#settings' ? 'active' : ''; ?>">
-                        Settings
-                    </a>
-                </li>
+
             </ul>
         </aside>
 
